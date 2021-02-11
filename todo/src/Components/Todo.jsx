a5b1c9246e40f4164dd0e0275437535be8473613
omnisniper <omnisniper2@gmail.com>
2021-02-11 02:49:54 -0600

    added component to todo.jsx and css for container

--- todo/src/Components/Todo.jsx
+++ todo/src/Components/Todo.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 function Todo(props) {
     return (
         <div>
             {props.item}
+            
         </div>
     )
 }
+class TodoList extends Component {
+    render() {
+      return (
+        <div className="todoListMain">
+          <div className="header">
+            <form>
+              <input placeholder="enter task">
+              </input>
+              <button type="submit">add</button>
+            </form>
+          </div>
+        </div>
+      );
+    }
+  }
 
 export default Todo;
 
--- todo/src/index.js
+++ todo/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var destination = document.querySelector("#container");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
