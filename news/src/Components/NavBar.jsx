98708a2877fc3aac52ceb3b9387b1ba2ab3a5ed7
omnisniper <omnisniper2@gmail.com>
2021-02-12 22:17:52 -0600

    added nav bar structure

--- news/src/Components/NavBar.jsx
+++ news/src/Components/NavBar.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 function NavBar() {
     return (
         <nav>
+            <Link to="/"></Link>
             <h1>TEK News</h1>
-            <span>Add your &lt;Link/&gt;'s to the Pages Here in the NavBar</span>
-            <p>You will need to use React Router's Navigation system as opposed to 'a' tags to achieve proper client side routing</p>
             <ul>
-                <li><a href="http://bit.ly/teksysacademy" target="_blank" rel="noreferrer">Sports</a></li>
-                <li><a href="http://bit.ly/teksysacademy" target="_blank" rel="noreferrer">Weather</a></li>
-                <li><a href="http://bit.ly/teksysacademy" target="_blank" rel="noreferrer">Politics</a></li>
+                 <li><Link to="/Headlines">Headlines</Link></li>
+                 <li><Link to="/Sports">Sports</Link></li>
+                 <li><Link to="/Weather">Weather</Link></li>
             </ul>
         </nav>
     )
